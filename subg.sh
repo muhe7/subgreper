93d4fd87a67e7393f8d5f1529dd60e1b0dd616e6
_m.u.h.e_ <72880007+mechb0y@users.noreply.github.com>
2021-05-28 11:03:28 +0530

    Update subg.sh

--- subg.sh
+++ subg.sh
@@ -12,7 +12,7 @@ echo  "${red} Subdomain Enumuration ${reset}"
 echo '---------------------------------------------'
 amass enum -d $1  > $dir/file.txt;
 subfinder -d $1 >> $dir/file.txt;
-python3 /root/Tools/github-search/github-subdomains.py -t 7da6812ba03d7ee7de9c022e9dcb5c19632ce642 -d $1 >> $dir/file.txt;
+python3 /root/Tools/github-search/github-subdomains.py -t your-github-token -d $1 >> $dir/file.txt;
 cat $dir/file.txt | grep $1 | sort -u > $dir/_subdomain;
 cat $dir/_subdomain | httpx -silent | tee $dir/test_resolved;
 bash /root/Tools/subzzZ/subzzZ -d $1 -s | sed 's/:8443$//' |  sed 's/:81$//' |  sed 's/:80$//' |  sed 's/:443$//' | sed 's/:8080$//' |  sed 's/:8000$//' |  sed 's/:10000$//' | sed 's/:9000$//' >> $dir/test_resolved
